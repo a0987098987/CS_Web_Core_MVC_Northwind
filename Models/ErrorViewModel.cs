cd5d46af1ca8536b393adc98521b330c9aede03f
Derek Li <a0987098987@hotmail.com>
2020-11-01 13:40:54 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CS_Web_Core_MVC_Northwind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
